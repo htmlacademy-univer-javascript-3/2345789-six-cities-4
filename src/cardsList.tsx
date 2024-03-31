ac0bf3edd200d9fc28361be64636fe4fcb35f09f
Oleg Kalashnikov <s1ckoleg@mail.ru>
2024-03-31 18:48:55 +0300

    implement filtering

--- src/cardsList.tsx
+++ src/cardsList.tsx
@@ -1,15 +1,31 @@
 import { CardProps } from './types/cardProps';
 import CardArticle from './card';
+import { filters } from './const';
 
 
 type CardsListProps = {
-    cards: CardProps[];
+    cards: CardProps[] & {rating: number}[];
+    sortType: string;
 }
 
-function CardsList({cards}: CardsListProps): JSX.Element {
+function CardsList({cards, sortType}: CardsListProps): JSX.Element {
+  let sortedCards: CardProps[];
+  switch (sortType) {
+    case filters.LOW_TO_HIGH:
+      sortedCards = [...cards].sort((a, b) => a.price - b.price);
+      break;
+    case filters.HIGH_TO_LOW:
+      sortedCards = [...cards].sort((a, b) => b.price - a.price);
+      break;
+    case filters.TOP_RATED:
+      sortedCards = [...cards].sort((a, b) => b.rating - a.rating);
+      break;
+    default:
+      sortedCards = cards;
+  }
   return (
     <>
-      {cards.map((item) => (
+      {sortedCards.map((item) => (
         <CardArticle key={item.id} id={item.id} price={item.price} isInBookmarks={item.isInBookmarks} roomName={item.roomName} roomType={item.roomType} onListItemHover={item.onListItemHover} />
       ))}
     </>
