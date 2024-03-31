81738fd4d99f71a6fde54183a650900be41406d3
Oleg Kalashnikov <s1ckoleg@mail.ru>
2024-03-31 17:52:28 +0300

    add redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,20 @@
 import Header from './header.tsx';
-import { Offer } from './types/offers.ts';
 import CardsList from './cardsList.tsx';
 import Map from './map.tsx';
 import { amsterdam } from './mocks/city.ts';
 import { useState } from 'react';
 import { Point } from './types/map.ts';
+import { useAppDispatch } from './hooks/index.ts';
+import { updateOffers } from './store/action.ts';
+import { store } from './store/index.ts';
 
-type MainProps = {
-    placesToStay: string;
-    offers: Offer[];
-}
 
-function MainScreen({placesToStay, offers}: MainProps): JSX.Element {
-  const points = offers.map((item) => ({
+function MainScreen(): JSX.Element {
+  const [currentState, setCurrentState] = useState(store.getState());
+  const handleCurrentState = () => {
+    setCurrentState(store.getState());
+  };
+  const points = currentState.offers.map((item) => ({
     title: item.name,
     lat: item.coordinates[0],
     lng: item.coordinates[1]
@@ -22,6 +24,7 @@ function MainScreen({placesToStay, offers}: MainProps): JSX.Element {
     const currentPoint = points.find((point) => point.title === listItemName);
     setSelectedPoint(currentPoint);
   };
+  const dispatch = useAppDispatch();
   return (
     <div className="page page--gray page--main">
 
@@ -33,32 +36,56 @@ function MainScreen({placesToStay, offers}: MainProps): JSX.Element {
           <section className="locations container">
             <ul className="locationslist tabslist">
               <li className="locations__item">
-                <a className="locationsitem-link tabsitem" href="#">
+                <a className={currentState.city === 'Paris' ? 'locationsitem-link tabsitem tabs__item--active' : 'locationsitem-link tabsitem'} href="#" onClick={() => {
+                  dispatch(updateOffers('Paris'));
+                  handleCurrentState();
+                }}
+                >
                   <span>Paris</span>
                 </a>
               </li>
               <li className="locations__item">
-                <a className="locationsitem-link tabsitem" href="#">
+                <a className={currentState.city === 'Cologne' ? 'locationsitem-link tabsitem tabs__item--active' : 'locationsitem-link tabsitem'} href="#" onClick={() => {
+                  dispatch(updateOffers('Cologne'));
+                  handleCurrentState();
+                }}
+                >
                   <span>Cologne</span>
                 </a>
               </li>
               <li className="locations__item">
-                <a className="locationsitem-link tabsitem" href="#">
+                <a className={currentState.city === 'Brussels' ? 'locationsitem-link tabsitem tabs__item--active' : 'locationsitem-link tabsitem'} href="#" onClick={() => {
+                  dispatch(updateOffers('Brussels'));
+                  handleCurrentState();
+                }}
+                >
                   <span>Brussels</span>
                 </a>
               </li>
               <li className="locations__item">
-                <a className="locationsitem-link tabsitem tabs__item--active">
+                <a className={currentState.city === 'Amsterdam' ? 'locationsitem-link tabsitem tabs__item--active' : 'locationsitem-link tabsitem'} onClick={() => {
+                  dispatch(updateOffers('Amsterdam'));
+                  handleCurrentState();
+                }}
+                >
                   <span>Amsterdam</span>
                 </a>
               </li>
               <li className="locations__item">
-                <a className="locationsitem-link tabsitem" href="#">
+                <a className={currentState.city === 'Hamburg' ? 'locationsitem-link tabsitem tabs__item--active' : 'locationsitem-link tabsitem'} href="#" onClick={() => {
+                  dispatch(updateOffers('Hamburg'));
+                  handleCurrentState();
+                }}
+                >
                   <span>Hamburg</span>
                 </a>
               </li>
               <li className="locations__item">
-                <a className="locationsitem-link tabsitem" href="#">
+                <a className={currentState.city === 'Dusseldorf' ? 'locationsitem-link tabsitem tabs__item--active' : 'locationsitem-link tabsitem'} href="#" onClick={() => {
+                  dispatch(updateOffers('Dusseldorf'));
+                  handleCurrentState();
+                }}
+                >
                   <span>Dusseldorf</span>
                 </a>
               </li>
@@ -70,7 +97,7 @@ function MainScreen({placesToStay, offers}: MainProps): JSX.Element {
           <div className="cities__places-container container">
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">{placesToStay} places to stay in Amsterdam</b>
+              <b className="places__found">{currentState.offers.length.toString()} places to stay in {currentState.city}</b>
               <form className="places__sorting" action="#" method="get">
                 <span className="places__sorting-caption">Sort by</span>
                 <span className="places__sorting-type" tabIndex={0}>
@@ -87,12 +114,12 @@ function MainScreen({placesToStay, offers}: MainProps): JSX.Element {
                 </ul>
               </form>
               <div className="cities__places-list places__list tabs__content">
-                <CardsList cards={offers.map((item) => ({id: item.id, price: item.price, isInBookmarks: item.isInBookmarks, roomName: item.name, roomType: item.features[0], onListItemHover: handleListItemHover}))}/>
+                <CardsList cards={currentState.offers.map((item) => ({id: item.id, price: item.price, isInBookmarks: item.isInBookmarks, roomName: item.name, roomType: item.features[0], onListItemHover: handleListItemHover}))}/>
               </div>
             </section>
             <div className="cities__right-section">
               <section className="cities__map map">
-                <Map city={amsterdam} points={points} selectedPoint={selectedPoint} height='800px' width='515px' />
+                <Map city={amsterdam} points={points} selectedPoint={selectedPoint} height='800px' width='515px' /> # пока захардкожено
               </section>
             </div>
           </div>
