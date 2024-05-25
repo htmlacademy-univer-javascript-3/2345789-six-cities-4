7d812e84e867ff7b9ff2c531dae9e3b2cbfe6fa6
Oleg Kalashnikov <s1ckoleg@mail.ru>
2024-05-25 16:29:23 +0300

    fix api name from debug

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
 import { getToken } from '../token';
 
-const BACKEND_URL = 'https://14.design.htmlacademy.pro/six-citiesss';
+const BACKEND_URL = 'https://14.design.htmlacademy.pro/six-cities';
 const REQUEST_TIMEOUT = 5000;
 
 export const createAPI = (): AxiosInstance => {
